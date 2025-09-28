688ee966a513b97067f05e10810b389bb82b50be
Shradha Shrivastava <142050894+lylols@users.noreply.github.com>
2025-09-28 16:10:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stk;
+        for(char ch : s){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stk.push(ch);
+            }
+            else{
+                if(stk.empty() || stk.top() == '(' && ch == ')' ||
+                    stk.top() == '[' && ch == ']' ||
+                    stk.top() == '{' && ch == '}'){
+                    stk.pop();
+                }
+                else return false;
+            }
+        }
+        return stk.empty();
+    }
+};
