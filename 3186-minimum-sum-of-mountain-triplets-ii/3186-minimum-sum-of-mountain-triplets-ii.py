bfa3a6ffc68c8938132c9c6ea764808f49f8c1c4
Shradha Shrivastava <142050894+lylols@users.noreply.github.com>
2025-10-09 15:48:42 +0530

    Time: 131 ms (85.56%), Space: 33.9 MB (7.49%) - LeetHub

--- 3186-minimum-sum-of-mountain-triplets-ii/3186-minimum-sum-of-mountain-triplets-ii.py
+++ 3186-minimum-sum-of-mountain-triplets-ii/3186-minimum-sum-of-mountain-triplets-ii.py
@@ -1,27 +1,18 @@
 class Solution:
     def minimumSum(self, nums: List[int]) -> int:
         n = len(nums)
-        left_min = [float('inf')] * n
-        right_min = [float('inf')] * n
+        suf_min = [0] * n
+        suf_min[-1] = nums[-1]
 
-        # Compute left smaller
-        min_left = nums[0]
-        for i in range(1, n):
-            if min_left < nums[i]:    
-                left_min[i] = min_left
-            min_left = min(min_left, nums[i])
-
-        # Compute right smaller
-        min_right = nums[-1]
         for i in range(n-2, -1, -1):
-            if min_right < nums[i]:     
-                right_min[i] = min_right
-            min_right = min(min_right, nums[i])
-
-        # Find minimum sum
-        min_sum = float('inf')
-        for i in range(1, n-1):
-            if left_min[i] != float('inf') and right_min[i] != float('inf'):
-                min_sum = min(min_sum, nums[i] + left_min[i] + right_min[i])
-
-        return min_sum if min_sum != float('inf') else -1
+            suf_min[i] = min(nums[i], suf_min[i+1])
+        
+        preMin = nums[0]
+        ans = inf
+        for j in range(1, n-1):
+            if preMin < nums[j] > suf_min[j+1]:
+                ans = min(ans, preMin + nums[j] + suf_min[j+1])
+            
+            preMin = min(preMin, nums[j])
+        
+        return ans if ans != inf else -1
