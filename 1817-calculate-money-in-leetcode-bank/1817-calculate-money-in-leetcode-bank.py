6718787b1e00d449d02c269199cd7d4afc0e5eac
Shradha Shrivastava <142050894+lylols@users.noreply.github.com>
2025-10-25 18:33:11 +0530

    Time: 0 ms (100%), Space: 18 MB (16.22%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
@@ -4,9 +4,7 @@ class Solution:
         m = n//7
         summ+= 28*m 
         i =1
-        while i<m:
-            summ+= i*7
-            i+=1    
+        summ += 7 * ((m - 1) * m) // 2   
         r= n%7
         summ+= ((r*(r+1))//2) +m*r
         return summ
