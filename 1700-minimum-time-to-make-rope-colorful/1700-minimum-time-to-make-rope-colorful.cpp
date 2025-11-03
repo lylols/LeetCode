bbc2d8ca86056486f77bdf4b43ce9c98f6c50126
Shradha Shrivastava <142050894+lylols@users.noreply.github.com>
2025-11-03 11:39:03 +0530

    Time: 11 ms (21.8%), Space: 99.8 MB (65.59%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    int minCost(string colors, vector<int>& neededTime) {
-        stack<pair<char,int>> stk; // store {color, time}
-        int total = 0;
-
-        for (int i = 0; i < colors.size(); i++) {
-            if (!stk.empty() && stk.top().first == colors[i]) {
-                total += min(stk.top().second, neededTime[i]); 
-                stk.top().second = max(stk.top().second, neededTime[i]); 
-            } else {
-                stk.push({colors[i], neededTime[i]});
+    int minCost(string col, vector<int>& time) {
+        int n = col.size(), sum = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (col[i] == col[i - 1]) {
+                sum += min(time[i], time[i - 1]);
+                time[i] = max(time[i], time[i - 1]);
             }
         }
-        return total;
+        return sum;
     }
 };
