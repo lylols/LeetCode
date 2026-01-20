33ff18784418a81dd661e581333ea090fb52ef20
Shradha Shrivastava <142050894+lylols@users.noreply.github.com>
2026-01-20 19:39:19 +0530

    Time: 0 ms (100%), Space: 19.4 MB (8.64%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.py
@@ -0,0 +1,26 @@
+class Solution:
+    def largestInteger(self, num: int) -> int:
+        even =[]
+        odd = []
+        digits = [int(d) for d in str(num)]
+
+        for d in digits:
+            if d % 2 == 0:
+                even.append(d)
+            else: odd.append(d)
+
+        even.sort(reverse=True)
+        odd.sort(reverse=True)
+
+        # Reconstruct result with parity preserved 
+        res = [] 
+        ei, oi = 0, 0
+        for d in digits:
+            if d % 2 == 0:
+                res.append(even[ei])
+                ei += 1
+            else:
+                res.append(odd[oi])
+                oi += 1
+
+        return int("".join(str(i) for i in res))
